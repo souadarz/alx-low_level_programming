fa2d0379a35239429f7763de6e7395a40a839969
souadarz <arziki.souad@gmail.com>
2023-06-16 18:46:20 -0700

    the alphabet at reverse

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - Prints the alphabet
+ *
+ * Description: prints the alphabet at reverse
+ * Return: 0
+ */
+
+int main(void)
+{
+	char letter;
+
+	for (letter = 'z'; letter <= 'a'; letter++)
+		putchar(letter);
+
+	putchar('\n');
+	return (0);
+}
