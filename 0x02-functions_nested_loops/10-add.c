214c67d56896eba0d38265e260c2c834ac36ed95
souadarz <arziki.souad@gmail.com>
2023-06-20 20:01:30 -0700

    adds two value

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+  * add - adds two integers
+  * @n: The first number to be computed
+  * @m: The second number to be computed
+  *
+  * Return: n + m
+  */
+int add(int n, int m)
+{
+	return (n + m);
+}
