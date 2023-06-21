944c52e360a66372458f405fd2be9c2bd4318b34
souadarz <arziki.souad@gmail.com>
2023-06-21 16:07:07 -0700

    function that prints the 9 times table

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -1,6 +1,43 @@
 #include "main.h"
 
 /**
+ * print_number - prints a number
+ * @n: the number to be printed
+ *
+ * Description: function that prints a number
+ * Return: void
+ */
+
+void print_number(int n)
+{
+	int digit, k, l;
+
+	l = n;
+	k = 1;
+	if (n == 0)
+	{
+		_putchar('0');
+		return;
+	}
+
+	while (l > 0)
+	{
+		k *= 10;
+		l = l / 10;
+	}
+
+	k = k / 10;
+
+	while (k >= 1)
+	{
+		digit = n / k;
+		_putchar('0' + digit);
+		n = n % k;
+		k = k / 10;
+	}
+}
+
+/**
  * times_table -  prints the 9 times table
  *
  * Description: function that prints the 9 times table
@@ -9,23 +46,22 @@
 
 void times_table(void)
 {
-	int c, l, n;
+	int c, l, n, s = 9;
 
-	for (c = 0; c <= 9; c++)
+	for (c = 0; c <= s; c++)
 	{
-		for (l = 0; l <= 9; l++)
+		for (l = 0; l <= s; l++)
 		{
 			n = c * l;
-			_putchar('0' + n);
+			print_number(n);
 
-			if (c != 9)
+			if (l != s)
 			{
 				_putchar(',');
 				_putchar(' ');
 			}
 			else
 			{
-				_putchar('$');
 				_putchar('\n');
 			}
 		}
