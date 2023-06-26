c08bde541b976fd1be0f1e3761da913cbb103530
souadarz <arziki.souad@gmail.com>
2023-06-26 14:17:26 -0700

    function that returns the length of a string

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -12,7 +12,7 @@ int _strlen(char *s)
 {
 	int j;
 
-	for (j = 1; s[j] != '\0'; j++)
+	for (j = 0; s[j] != '\0'; j++)
 	{}
 
 	return (j);
---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * reset_to_98 - updates the value of a pointer to points to to 98
+ * @n: the integer to be computed
+ *
+ * Desciption:function that updates the value of a pointer to points to to 98
+ * Return: Always 0.
+ */
+
+void reset_to_98(int *n)
+{
+
+	*n = 98;
+}
