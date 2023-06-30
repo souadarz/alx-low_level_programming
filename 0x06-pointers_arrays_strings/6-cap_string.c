02e102326fb0569926da4f484d4bcdfcb8c417cd
souadarz <arziki.souad@gmail.com>
2023-06-29 19:58:00 -0700

    function that capitalizes all words of a string

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,37 @@
+#include "main.h"
+
+/**
+ * cap_string - copies a string
+ * @str: string to be copie
+ *
+ * Description: function that copies a string
+ * Return: char
+ */
+
+char *cap_string(char *str)
+{
+	int i, j;
+	int foundSep = 1;
+	char sep[] = ",;.!?\"(){}\n\t\" ";
+
+	for (i = 0; str[i] != '\0'; i++)
+	{
+
+		if (str[i] >= 'a' && str[i] <= 'z')
+		{
+			if (foundSep)
+				str[i] -= 'a' - 'A';
+		}
+
+		for (j = 0; sep[j] != '\0'; j++)
+		{
+			if (str[i] == sep[j])
+				foundSep = 1;
+
+			else
+				foundSep = 0;
+		}
+	}
+
+	return (str);
+}
