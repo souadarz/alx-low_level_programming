bb5465ecd3b2c32255c38a2eb09d42e5c717c17d
souadarz <arziki.souad@gmail.com>
2023-07-07 17:20:41 -0700

    program that prints its name

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -2,10 +2,11 @@
 
 /**
  * main - fonction that prints its name
- * argc: argument count
- * argv: argument vector
+ * @argc: argument count
+ * @argv: argument vector
  * Return: 0
  */
+
 int main(int argc __attribute__((unused)), char *argv[])
 {
 	printf("%s\n", argv[0]);
