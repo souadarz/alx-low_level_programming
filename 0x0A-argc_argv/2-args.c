0898fbe2539b40900ada8304d9b7ed6132efd1a8
souadarz <arziki.souad@gmail.com>
2023-07-07 17:27:19 -0700

    program that prints all arguments it receives

--- 0x0A-argc_argv/2-args.c
+++ 0x0A-argc_argv/2-args.c
@@ -2,8 +2,8 @@
 
 /**
  * main - fonction that prints all arguments it receives
- * argc: argument count
- * argv: argument vector
+ * @argc: argument count
+ * @argv: argument vector
  * Return: 0
  */
 
