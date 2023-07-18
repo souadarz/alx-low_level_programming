f55ae27afa7892502944f4cd7de8576e57c2a865
souadarz <arziki.souad@gmail.com>
2023-07-17 19:54:37 -0700

    task 3

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -16,6 +16,11 @@ typedef struct dog
 	float age;
 	char *owner;
 } dog;
+
+/**
+ * dog_t - new name for the type struct dog
+ */
+typedef struct dog dog_t;
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
 
