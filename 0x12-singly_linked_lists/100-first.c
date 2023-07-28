21680be5704456c84304f65d949e7d34caab90b2
souadarz <arziki.souad@gmail.com>
2023-07-28 13:42:28 -0700

    function that prints a statments before the main function is executed

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+void before_main() __attribute__((constructor));
+
+/**
+ * before_main - function that prints sentences
+ *                before the main function is executed.
+ *
+ * Return: nothings
+ */
+
+void before_main()
+{
+	printf("You're beat! and yet, you must allow,\n");
+	printf("I bore my house upon my back!\n");
+}
