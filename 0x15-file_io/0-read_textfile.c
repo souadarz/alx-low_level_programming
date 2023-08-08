867b9bf8f05f8d282806533fffc05352bdd1abf6
souadarz <arziki.souad@gmail.com>
2023-08-07 17:17:22 -0700

    function that reads a text file and prints it to the POSIX standard output

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -32,8 +32,13 @@ ssize_t read_textfile(const char *filename, size_t letters)
 	}
 
 	count1 = read(file_d, ptr, letters);
+
+	if (count1 == -1)
+		return (0);
 	count2 = write(STDOUT_FILENO, ptr, count1);
 
+	if (count2 == 0)
+		return (0);
 	free(ptr);
 	close(file_d);
 	return (count2);
