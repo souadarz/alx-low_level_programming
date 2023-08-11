c22767a933cc99d3a170daf3aefdb5203c521e2a
souadarz <arziki.souad@gmail.com>
2023-08-11 07:18:45 -0700

    program that copies the content of a file to another file

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -3,45 +3,43 @@
 /**
  *main - that copies the content of a file to another file
  *@argc: number of arguments
- *@argv: the argument vector
+ *@av: the argument vector
  * Return:...
  */
 
-int main(int argc, char *argv[])
+int main(int argc, char *av[])
 {
-	int fd_1, fd_2, result1, result2;
 	char buffer[1024];
+	int fd_1, fd_2, result1, result2;
 
 	if (argc != 3)
 		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n"), exit(97);
 
-	fd_1 = open(argv[1], O_RDONLY);
+	if (av[1] == NULL)
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", av[1]), exit(98);
+
+	if (av[2] == NULL)
+		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", av[2]), exit(99);
+	fd_1 = open(av[1], O_RDONLY);
 
 	if (fd_1 < 0)
-	{
-		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
-		exit(98);
-	}
-	fd_2 = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC, 0664);
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", av[1]), exit(98);
+	fd_2 = open(av[2], O_WRONLY | O_CREAT | O_TRUNC, 0664);
 
 	if (fd_2 < 0)
-		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]), exit(99);
+		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", av[2]), exit(99);
 
 	while (result1)
 	{
 		result1 = read(fd_1, buffer, 1024);
 
 		if (result1 < 0)
-		{
-			dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
-			exit(98);
-		}
+			dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", av[1]), exit(98);
 		result2 = write(fd_2, buffer, result1);
 
 		if (result2 < 0)
-			dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]), exit(99);
+			dprintf(STDERR_FILENO, "Error: Can't write to %s\n", av[2]), exit(99);
 	}
-
 	close(fd_1);
 
 	if (close(fd_1) < 0)
