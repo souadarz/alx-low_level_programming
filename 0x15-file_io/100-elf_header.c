2e8847ff43d9c9df4d28d7ad97e8a1cbc0dfb1e9
souad <arziki.souad@gmail.com>
2023-09-04 18:53:05 -0700

    program that displays the information contained in the ELF header at the start of an ELF file

---
+++ 0x15-file_io/100-elf_header.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - the entry point to program that displays the information
+ *        contained in the ELF header at the start of an ELF file.
+ *Return: ...
+ */
+
+int main(void)
+{
+	exit(98);
+	return (0);
+}
